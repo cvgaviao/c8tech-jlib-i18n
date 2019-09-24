6d65276a9e06c0180346cab931c5f878fbe378ed
Cristiano V. Gavião <cvgaviao@gmail.com>
2019-09-24 14:21:47 -0300

    added the main i18n annotations

---
+++ src/main/java/br/com/c8tech/javalib/apt/i18n/Message.java
@@ -0,0 +1,41 @@
+/**
+ * ============================================================================
+ *  Copyright ©  2015-2019,    Cristiano V. Gavião
+ *
+ *  All rights reserved.
+ *  This program and the accompanying materials are made available under
+ *  the terms of the Eclipse Public License v1.0 which accompanies this
+ *  distribution and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * ============================================================================
+ */
+package br.com.c8tech.javalib.apt.i18n;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(SOURCE)
+@Target(METHOD)
+public @interface Message {
+
+    /**
+     * 
+     * @return An optional key for the message.
+     */
+    String key() default "";
+
+    /**
+     * 
+     * @return A required message template.
+     */
+    String value();
+
+    /**
+     * 
+     * @return The default locale of this message.
+     */
+    String locale() default "en-US";
+}
